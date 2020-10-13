5595c9fc0dc58f37a066d1c0d1661188cdd25324
st0n3 <ssst0n3@gmail.com>
2020-10-13 13:53:09 +0800

    修改：     package.json 	修改：     src/util/api.js

--- src/util/api.js
+++ src/util/api.js
@@ -12,71 +12,68 @@ export default {
         this.config.baseURL = baseURL
         this.client = axios.create(this.config)
     },
-    async exec(method, path, params, data, caller, extra_msg) {
-        console.log("caller:", caller)
+    async exec(method, path, params, data, toast_options) {
+        if (toast_options) {
+            console.log("caller:", toast_options.caller)
+            if (toast_options.error_msg === undefined) {
+                toast_options.error_msg = ""
+            }
+            if (toast_options.success_msg === undefined) {
+                toast_options.success_msg = ""
+            }
+        }
+
         return this.client({
             url: path, method: method, params: params, data: data
         }).then(req => {
-            if (caller) {
-                console.log("msg:", req.data.msg)
-                if (req.data.msg) {
-                    let msg = req.data.msg
-                    if (extra_msg) {
-                        msg += extra_msg
-                    }
-                    caller.$bvToast.toast(msg, {
-                        title: path,
-                        variant: 'success',
-                        solid: true
-                    })
-                }
+            if (toast_options && toast_options.caller) {
+                let msg = req.data.msg ? req.data.msg + toast_options.success_msg : toast_options.success_msg
+                console.log("msg:", msg)
+                toast_options.caller.$bvToast.toast(msg, {
+                    title: path,
+                    variant: 'success',
+                    solid: true
+                })
             }
             return req.data
         }).catch(function (error) {
-            if (error.response) {
-                if (error.response.data) {
-                    if (caller) {
-                        let msg = error.response.data.reason
-                        if (extra_msg) {
-                            msg += extra_msg
-                        }
-                        caller.$bvToast.toast(msg, {
-                            title: path,
-                            variant: 'danger',
-                            solid: true
-                        })
-                    }
-                    return error.response.data
-                }
-                return error.response
+            let ret = error.response ? (error.response.data ? error.response.data : error.response) : error
+            if (toast_options.caller) {
+                let msg = error.response ? (error.response.data ? error.response.data.reason : "") + toast_options.error_msg : toast_options.error_msg
+                console.log("msg:", msg)
+                toast_options.caller.$bvToast.toast(msg, {
+                    title: path,
+                    variant: 'danger',
+                    solid: true
+                })
             }
-            return error
+            return ret
         })
     },
-    async get(path, params, caller, extra_msg) {
-        return this.exec('get', path, params, null, caller, extra_msg)
+    async get(path, params, toast_options) {
+        return this.exec('get', path, params, null, toast_options)
     },
-    async post(path, params, data, caller, extra_msg) {
-        return this.exec('post', path, params, data, caller, extra_msg)
+    async post(path, params, data, toast_options) {
+        return this.exec('post', path, params, data, toast_options)
     },
-    async put(path, params, data, caller, extra_msg) {
-        return this.exec('put', path, params, data, caller, extra_msg)
+    async put(path, params, data, toast_options) {
+        return this.exec('put', path, params, data, toast_options)
     },
-    async delete(path, params, caller, extra_msg) {
-        return this.exec('delete', path, params, null, caller, extra_msg)
+    async delete(path, params, toast_options) {
+        return this.exec('delete', path, params, null, toast_options)
     },
-    async listResource(path, caller, extra_msg) {
-        return this.get(path, null, caller, extra_msg)
+    async listResource(path, toast_options) {
+        return this.get(path, null, toast_options)
     },
-    async createResource(path, data, caller, extra_msg) {
-        return this.post(path, null, data, caller, extra_msg)
+    async createResource(path, data, toast_options) {
+        return this.post(path, null, data, toast_options)
     },
-    async updateResource(path, id, data, caller, extra_msg) {
-        return this.put(path + '/' + id, null, data, caller, extra_msg)
+    async updateResource(path, id, data, toast_options) {
+        return this.put(path + '/' + id, null, data, toast_options)
     },
-    async deleteResource(path, id, caller, extra_msg) {
+    async deleteResource(path, id, toast_options) {
         if (confirm('Are you sure you want to delete it?')) {
-            await this.delete(`${path}/${id}`, null, caller, extra_msg);
+            await this.delete(`${path}/${id}`, null, toast_options);
             location.reload()
         }
     },
